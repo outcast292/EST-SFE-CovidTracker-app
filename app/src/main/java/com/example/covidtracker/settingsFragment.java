48fd3431e4535465f962b2c783451a40d0fc080c
Hatim <hatimgbl@gmail.com>
2020-05-01 03:16:45 -0700

    added notification on notifs tab / activation push notification

---
+++ app/src/main/java/com/example/covidtracker/settingsFragment.java
@@ -0,0 +1,37 @@
+package com.example.covidtracker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class settingsFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.settings_fragment, container, false);
+
+        return view;
+    }
+
+}
